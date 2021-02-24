2145703d438da7065dc3ba875488d394b50fc387
Bruno A. Brunckhorst <brunobrunckhorst@gmail.com>
2021-02-24 20:27:08 +0100

    Changed script name

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='sample_package',
-      version="1.0",
-      description="Project Description",
-      packages=find_packages(),
-      install_requires=requirements,
-      test_suite='tests',
-      # include_package_data: to install data from MANIFEST.in
-      include_package_data=True,
-      scripts=['scripts/sample_package-run'],
-      zip_safe=False)
+setup(
+    name='sample_package',
+    version="1.0",
+    description="A sample package that does a few cool things.",
+    packages=find_packages(),
+    install_requires=requirements,
+    test_suite='tests',
+    # include_package_data: to install data from MANIFEST.in
+    include_package_data=True,
+    scripts=['scripts/sample_package-computedist'],
+    zip_safe=False
+)
